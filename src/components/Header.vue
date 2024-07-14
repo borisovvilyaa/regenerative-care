25689eefc90d4f7c1c573ab488af677362491c2c
borisovvilyaa <borisovvilyaa@gmail.com>
2024-07-14 12:21:48 +0300

    add shadow

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -101,6 +101,10 @@ export default {
 </script>
 
 <style scoped>
+header {
+  box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+    rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+}
 .navbar {
   position: fixed;
   top: 0;
